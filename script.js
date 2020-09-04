f1c91d5fede1a27dc3a2a4872fed98fc8e39b711
Alexandra <alexandra@Alexandras-MacBook-Pro.local>
2020-09-04 10:19:56 +0200

    changes in html, css and js

--- script.js
+++ script.js
@@ -6,14 +6,26 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
   .then(function (data) {
     data.forEach(showOneCourse);
   });
-
 function showOneCourse(course) {
   const templateContent = document.querySelector("#courseTemplate").content;
   const copy = templateContent.cloneNode(true);
+
+  //just seeing it in the console
+  console.log(course.discount);
+  console.log(course);
+
   copy.querySelector(".card_heading").textContent = course.name;
-  copy.querySelector(".card_desc").textContent = course.shortDescription;
+  copy.querySelector(".short_desc").textContent = course.shortdescription;
   copy.querySelector("img").src =
     "https://kea-alt-del.dk/t5/site/imgs/small/" + course.image + "-sm.jpg";
+  if (course.vegetarian) {
+    copy.querySelector(".card_veg").classList.remove("hidden");
+  } else {
+    copy.querySelector(".card_meat").classList.remove("hidden");
+  }
+  if (course.soldout) {
+    copy.querySelector(".sold_out").classList.remove("hidden");
+  }
   copy.querySelector(".price").textContent = course.price + ",-";
-  document.querySelector("main").appendChild(copy);
+  document.querySelector(".productlist").appendChild(copy);
 }
